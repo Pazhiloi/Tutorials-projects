843a376eea675ce33217e7c011e3337c16e34c35
Miroslav Miroslavov <martinsen06@gmail.com>
2024-04-16 15:48:11 +0300

    25 A simple movement script for a character

---
+++ TutTemplateHypCasual/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+  private Touch touch;
+  public float speed;
+  public float posXmin;
+  public float posXmax;
+
+
+  private void Update()
+  {
+    if (Input.touchCount > 0)
+    {
+      touch = Input.GetTouch(0);
+
+      if (touch.phase == TouchPhase.Moved)
+      {
+        transform.position = new Vector3(transform.position.x + touch.deltaPosition.x * speed,
+         transform.position.y,
+         transform.position.z
+         );
+      }
+
+      if (transform.position.x < posXmin)
+      {
+        transform.position = new Vector3(posXmin, transform.position.y, transform.position.z);
+      }
+      if (transform.position.x > posXmax)
+      {
+        transform.position = new Vector3(posXmax, transform.position.y, transform.position.z);
+      }
+    }
+  }
+}
